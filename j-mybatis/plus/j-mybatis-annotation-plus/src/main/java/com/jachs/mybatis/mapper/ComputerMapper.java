9ec1ba66359a7d16e55bd64695f599b60d8efa5f
jachs <799516423@qq.com>
2022-07-06 10:08:22 +0800

    wqe

--- j-mybatis/plus/j-mybatis-annotation-plus/src/main/java/com/jachs/mybatis/mapper/ComputerMapper.java
+++ j-mybatis/plus/j-mybatis-annotation-plus/src/main/java/com/jachs/mybatis/mapper/ComputerMapper.java
@@ -2,6 +2,12 @@ package com.jachs.mybatis.mapper;
 
 import com.jachs.mybatis.domain.Computer;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.github.yulichang.base.MPJBaseMapper;
 
 /**
@@ -13,5 +19,17 @@ import com.github.yulichang.base.MPJBaseMapper;
  * @since 2022-04-06
  */
 public interface ComputerMapper  extends MPJBaseMapper <Computer>{
+
+	List<Computer> selectPageByXml(@Param("price") Long price);
+
+	List<Computer> selectPageByXml(@Param("price") Long price, Page<Computer> page);
+
+	@Select(value = "SELECT computerId,computerName,computerPrice,madeTime FROM computer\r\n"
+			+ "	     WHERE (computerPrice >= #{price}) ORDER BY computerPrice ASC")
+	List<Computer> selectPageByAnn(@Param("price") Long price);
+	
+	@Select(value = "SELECT computerId,computerName,computerPrice,madeTime FROM computer\r\n"
+			+ "	     WHERE (computerPrice >= #{price}) ORDER BY computerPrice ASC")
+	List<Computer> selectPageByAnn(@Param("price") Long price, Page<Computer> page);
 
 }
